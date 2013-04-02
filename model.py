7214488c962044b6c144096f8d0aa7b2b371dbb8
ChrUbuntu User <user@chromebox-005.(none)>
2013-04-01 18:25:33 -0700

    ratings movie flask app

--- model.py
+++ model.py
@@ -1,13 +1,66 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+	__tablename__ = "Users"
 
+	id = Column(Integer, primary_key = True)
+	email = Column(String(64), nullable = True)
+	password = Column(String(64), nullable = True)
+	age = Column(Integer, nullable = True)
+	zipcode = Column(String(15), nullable = True)
+
+	def __init__(self, email = None, password = None, age=None, zipcode=None):
+		self.email = email
+		self.password = password
+		self.age = age
+		self.zipcode = zipcode
+
+class Movie(Base):
+	__tablename__ = "Movies"
+
+	id = Column(Integer, primary_key = True)
+	name = Column(String(64), nullable = True)
+	released_at = Column(String(15), nullable = True)
+	imdb_url = Column(String(64), nullable = True)
+
+	def __init__(self, name = None, released_at = None, imdb_url = None):
+		self.name = name
+		self.released_at = released_at
+		self.imdb_url = imdb_url
+
+class Ratings(Base):
+	__tablename__ = "Ratings"
+
+	id = Column(Integer, primary_key = True)
+	movie_id = Column(Integer, ForeignKey('Movies.id'))
+	user_id = Column(Integer, ForeignKey('Users.id'))
+	rating = Column(Integer, nullable = True)
+
+	def ___init__(self, id = None, movie_id = None, user_id = None, rating = None):
+		self.movie_id = movie_id
+		self.user_id = user_id
+		self.rating = rating
 
 ### End class declarations
+
+def connect():
+	global ENGINE
+	global Session
+
+	ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+	Session = sessionmaker(bind=ENGINE)
+
+	return Session()
 
 def main():
     """In case we need this for something"""
